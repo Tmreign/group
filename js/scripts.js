99cd6d8b163e5e905cc2b8c51d9fde2167ab2535
tmreign <tomwanyoike9@gmail.com>
2019-06-19 11:21:36 +0300

    group

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+ 
+//  var typeOfErand =[];
+
+
+
+ 
+ function errand(book,data,proposal){
+     this.book=book;
+     this.data=data;
+     this.proposal=proposal;
+     this.price=0;
+
+ }
+
+
+
+ var time=['oneday','twoday','fiveday','onewk','twowk']
+
+ errand.prototype.finalOutput =function(){
+     if(this.book==[0] ||this.data==[0] || this.proposal==[0]){
+         this.price+=300;
+     }
+     else if(this.book==[1]||this.data==[1] || this.proposal==[1]){
+         this.price +=500;
+     }
+     else if(this.book==[2]||this.data==[2] || this.proposal==[2]){
+        this.price +=800;
+    }
+    else if(this.book==[3]||this.data==[3] || this.proposal==[3]){
+        this.price +=1000;
+    }
+    else if(this.book==[4]||this.data==[4] || this.proposal==[4]){
+        this.price +=1200;
+    }
+
+    return this.price;
+ }
+
+ errand.prototype.cost= function(){
+
+     var cost =0;
+     this.errand.forEach(function(errand){
+         
+        cost+= errand.cost
+
+     });
+         
+     this.cost =cost
+     
+ }
+
+
+
+ $(document).ready(function() {
+    $("#submit").submit(function(event) {
+        event.preventDefault();
+
+        var customErand = $("typeErrand").val();
+        var customTime = $("Time").val();
+        
+    $('#typeErrand').slideUp();
+    $('#Time').slideUp();
+  });
+});
